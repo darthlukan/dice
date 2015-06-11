0d4d80405d56770f854dc393dc4f0436120ff553
Brian Tomlinson <darthlukan@gmail.com>
2015-06-11 21:05:10 +0200

    this works on the cli, as long as you don't spam it within a nanosecond

--- main.go
+++ main.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"github.com/codegangsta/cli"
+	"math/rand"
 	"os"
+	"time"
 )
 
-func Dfour(qty int64) {}
+func randomInt64(n int64) int64 {
+	// rand.Int63n panics if n == 0
+	if n == 0 {
+		n = 1
+	}
+	return rand.Int63n(n) + 1 // Never want to return zero
+}
 
-func Dsix(qty int64) {}
+func sidesAreValid(sides int64) bool {
+	validSides := []int64{4, 6, 8, 10, 12, 20}
+	for _, vs := range validSides {
+		if vs == sides {
+			return true
+		}
+	}
+	return false
+}
 
-func Deight(qty int64) {}
+func Roll(sides, qty int64) int64 {
+	var val int64
+	for i := int64(1); i <= qty; i++ {
+		val += randomInt64(sides)
+	}
+	return val
+}
 
-func Dten(qty int64) {}
-
-func Dtwelve(qty int64) {}
-
-func Dtwenty(qty int64) {}
-
-func Roll(sides, qty int64) {}
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
 
 func main() {
 	app := cli.NewApp()
@@ -28,5 +47,28 @@ func main() {
 		Name:  "Brian Tomlinson",
 		Email: "darthlukan@gmail.com",
 	}}
+	app.Flags = []cli.Flag{
+		cli.IntFlag{
+			Name:  "sides, s",
+			Value: 0,
+			Usage: "The number of sides of the desired die.",
+		},
+		cli.IntFlag{
+			Name:  "qty, q",
+			Value: 0,
+			Usage: "The number of die to roll.",
+		},
+	}
+	app.Action = func(c *cli.Context) {
+		sides := int64(c.Int("sides"))
+		qty := int64(c.Int("qty"))
+
+		if sidesAreValid(sides) && qty >= 1 {
+			result := Roll(sides, qty)
+			fmt.Printf("%vd%v roll result: %v\n", qty, sides, result)
+		} else {
+			fmt.Printf("Please see 'dice --help' for usage assistance.\n")
+		}
+	}
 	app.Run(os.Args)
 }
