58365dede2fa8edca798c7ac4466935ffdee2893
Brian Tomlinson <darthlukan@gmail.com>
2015-06-11 19:44:46 +0200

    basic struture and usage of cli lib

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"github.com/codegangsta/cli"
+	"os"
+)
+
+func Dfour(qty int64) {}
+
+func Dsix(qty int64) {}
+
+func Deight(qty int64) {}
+
+func Dten(qty int64) {}
+
+func Dtwelve(qty int64) {}
+
+func Dtwenty(qty int64) {}
+
+func Roll(sides, qty int64) {}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "dice"
+	app.Version = "0.0.1"
+	app.Authors = []cli.Author{cli.Author{
+		Name:  "Brian Tomlinson",
+		Email: "darthlukan@gmail.com",
+	}}
+	app.Run(os.Args)
+}
